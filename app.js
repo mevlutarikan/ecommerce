da4ac569c9264e10c62aff108c08300ad8f5e1a2
Mevlut Arikan <mevlut@sektorbilisim.com>
2021-04-28 22:00:08 +0300

    Set routes, serve react entry point

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cookieParser());
+//Serve static frontend files from the React app build folder
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
+//app.use('/', indexRouter);
+app.use('/api', apiRouter);
+// Handle any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reactapp/build/index.html'));
+});
 
 module.exports = app;
