56237b1aac98a2da26cd9d97f2722dcde604df86
Mevlut Arikan <mevlut@sektorbilisim.com>
2021-04-29 19:02:04 +0300

    Add main dom routes-Signup,Login- and import layout

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import {Signup} from "./components/Signup";
+import {Layout} from "./components/Layout";
+import {Login} from "./components/Login";
+
 function App() {
   return (
-    <div className="App">
-        {/* <header /> */} 
-        <h1>E-store Shop</h1>
+    <Layout>
+      <Router>
+        <Switch>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
 
-    </div>
+          {/* <Route component={NotFound} /> */}
+        </Switch>
+      </Router>
+    </Layout>
   );
 }
 
