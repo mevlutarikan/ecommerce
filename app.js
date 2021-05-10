3f21a69e9a44a7ea26a782ff57c0c45a83174aa8
Mevlut Arikan <mevlut@sektorbilisim.com>
2021-05-10 13:15:49 +0300

    Fix cors policy
    Install cors package, add cors allowed site from enviroment variables.

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
-
+const cors = require('cors');
 var app = express();
 const dotenv = require('dotenv');
 
 // read environment variables
 dotenv.config();
+
+// cors allowed site
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 
